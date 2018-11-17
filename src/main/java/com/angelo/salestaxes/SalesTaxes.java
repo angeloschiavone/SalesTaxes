0fed7c105a3b9acd0fd831e0e6d47ed7a37662c2
SCHIAVONE Angelo <ASE10@dsone.3ds.com>
2018-11-17 12:03:38 +0100

    added project files

---
+++ src/main/java/com/angelo/salestaxes/SalesTaxes.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.angelo.salestaxes;
+
+/**
+ *
+ * @author ASE10
+ */
+public class SalesTaxes {
+    public static void main(String[] args)
+    {
+        System.out.println("Pippo");
+    }
+}
